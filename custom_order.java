3d185c1f5c88512ac5de939d5254e3d727314cec
Zheng-zhihan <66817683+Zheng-zhihan@users.noreply.github.com>
2020-06-13 12:26:44 +0800

    Update custom_order.java

--- custom_order.java
+++ custom_order.java
@@ -97,10 +97,16 @@ toppings = keyboard.nextLine();
       
       
     // TEST CODE
-      	System.out.println("You order the"+frostingType+"with"+fillingType+toppingType+",Yes or No?");
-	intput = keyboard.nextLine();
-    // STEP 9 DISPLAY ORDER CONFIRMATION
 
+    // STEP 9 DISPLAY ORDER CONFIRMATION
+System.out.println();
+System.out.println(firstName + " , your order is as follows: ");
+System.out.println("_________________________________________");
+System.out.println("Item Ordered: " + itemOrder);
+System.out.println("Frosting: " + frostingType);
+System.out.println("Filling: " + fillingType);
+System.out.println("Toppings: " + toppings);
+System.out.println("_________________________________________");
 
       
 	  
@@ -108,10 +114,10 @@ toppings = keyboard.nextLine();
 	  
     
     // TEST CODE
-	tax = cost*TAX_RATE
-      	System.out.println("You cost"+cost+"about"+frostingType);
-	System.out.println("the sales tax is"+tax);
-      }
+System.out.printf("The cost of your order is: $%.2f\n", cost);
+tax = cost * TAX_RATE;
+System.out.printf("The tax is: $%.2f\n", tax);
+System.out.printf("The total due is: $%.2f\n",(tax + cost));
     // STEP 10 DISPLAY COST AND SALES TAX
   
     }   
